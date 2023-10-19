d1f6410164fb0d214e3fee45de80fbab7af54853
Ken <kennethmorales847@gmail.com>
2023-10-18 18:24:28 -0600

    Creacion de tablas de Proveedores y Productos Terminados

--- proveedores_form.Designer.vb
+++ proveedores_form.Designer.vb
@@ -22,9 +22,16 @@ Partial Class proveedores_form
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'proveedores_form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1088, 427)
+        Me.Name = "proveedores_form"
         Me.Text = "proveedores_form"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
