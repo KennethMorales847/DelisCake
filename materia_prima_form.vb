248cb486ebd539d90d6e2c4d79557627269dd39f
Ken <kennethmorales847@gmail.com>
2023-10-19 13:08:58 -0600

    Se agregó un nuevo forms de materia prima con su respectiva base de datos

---
+++ materia_prima_form.vb
@@ -0,0 +1,7 @@
+﻿Public Class materia_prima_form
+    Private Sub materia_prima_form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'DelisCakeSet4.MP' table. You can move, or remove it, as needed.
+        Me.MPTableAdapter.Fill(Me.DelisCakeSet4.MP)
+
+    End Sub
+End Class
