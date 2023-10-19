5933d884c0d11e8aade46dd26ae0c0dc5d9d800a
Ken <kennethmorales847@gmail.com>
2023-10-19 17:27:31 -0600

    Se borró un error

--- materia_prima_form.vb
+++ materia_prima_form.vb
@@ -5,8 +5,4 @@
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.MPTableAdapter.agregar(Id_IngredienteTextBox.Text, NombreTextBox.Text, CategoriaTextBox.Text, Fecha_CompraDateTimePicker.Text, Fecha_VencimientoDateTimePicker.Text, Unidad_MedidaTextBox.Text, CantidadTextBox.Text, IVA_UnitarioTextBox.Text)
-        Me.MPTableAdapter.Fill(Me.DelisCakeSet4.MP)
-    End Sub
 End Class
